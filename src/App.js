83e2662178daa89ce735c749b5a0cbe5eb5f706d
Vishal Snate <vishalsnate@gmail.com>
2022-03-12 10:38:18 +0530

    final

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+import Home from './Components/Home';
+import Head from './Components/Head';
+
+function App() {
+  return (
+    <div className="App">
+      <Head />
+      <Home />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Components/Section.js
@@ -0,0 +1,104 @@
+import React from 'react'
+import styled from "styled-components"
+import { Fade } from "react-awesome-reveal";
+
+function Section({title,
+    description,
+    leftButtonText,
+    rightButtonText,
+    backgroundImage,
+}) {
+    return (
+        <Wrap bgImage={backgroundImage}>
+            <Fade bottom>
+            <ItemText>
+                <h1>{title}</h1>
+                <p>{description}</p>
+            </ItemText>
+            </Fade>
+            <Buttons>
+            <Fade bottom>
+                <ButtonGroup>
+                    <LeftButton>
+                        {leftButtonText}
+                    </LeftButton>
+                    {   rightButtonText &&
+                        <RightButton>
+                            {rightButtonText}
+                        </RightButton>
+                    }
+                </ButtonGroup>
+            </Fade>
+                <DownArrow src="/images/down-arrow.svg" />
+            </Buttons>
+        </Wrap>
+    )
+}
+
+export default Section
+
+const Wrap = styled.div`
+    z-index: 10;
+    width: 100vw;
+    height: 100vh;
+    background-size: cover;
+    background-position: center;
+    background-repeat: no-repeat;
+    background-image: url('/images/model-s.jpg');
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+    align-items: center;
+    background-image: ${props => `url("/images/${props.bgImage}")`}
+`
+
+const ItemText = styled.div`
+  padding-top: 15vh;
+  text-align: center;
+`
+
+
+
+const ButtonGroup = styled.div`
+    display: flex;
+    margin-bottom: 30px;
+    gap: 25px;
+  @media (max-width: 768px) {
+    flex-direction: column;
+`
+
+const LeftButton = styled.div`
+    cursor: pointer;
+    background-color: rgba(23, 26, 32, 0.8);
+    height: 40px;
+    width: 256px;
+    color: white;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    border-radius: 100px;
+    opacity: 0.85;
+    text-transform: uppercase;
+    font-size: 12px;
+    font-weight: bold;
+`
+
+const RightButton = styled(LeftButton)`
+    background-color: white;
+    opacity: 0.65;
+    color: black;
+`
+
+const DownArrow = styled.img`
+    margin-top: 20px;
+    height: 40px;
+    overflow-x:hidden;
+    animation: animateDown infinite 1.5s;
+`
+
+const Buttons = styled.div`
+    display: flex;
+    flex-direction: column;
+`
+
+///fade
